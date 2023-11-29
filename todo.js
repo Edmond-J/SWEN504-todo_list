b78dec69ad52b7e2030b08e013f25674fdfc0ab7
jinanme <jinanme@myvuw.ac.nz>
2023-11-30 10:34:05 +1300

    tick and untick a to-do item

--- todo.js
+++ todo.js
@@ -1,18 +1,42 @@
 let listItems = document.querySelectorAll(".listItem");
 let main = document.querySelector(".main");
-let task = document.querySelector(".task");
+let detail = document.querySelector(".detail");
 let menu = document.getElementById("menu");
 let back = document.getElementById("back");
 let newToDo = document.getElementById("newToDo");
 
 listItems.forEach((div) => {
+  //click a to-do item will show the details
+  let content = div.querySelector("div");
+  content.addEventListener("click", () => {
+    // task.classList.toggle('hidden');
+    if (detail.style.display === "none") {
+      detail.style.display = "block";
+    } else {
+      detail.style.display = "none";
+      console.log(div.textContent); //action on clicking each list item
+    }
+  });
   // check button action
   let imgCheck = div.querySelector("img");
   imgCheck.addEventListener("mouseover", function () {
-    imgCheck.src = "./icon/circle-check-regular.svg";
+    if (this.src.endsWith("circle-regular.svg")) {
+      imgCheck.src = "./icon/circle-check-regular.svg";
+    }
   });
   imgCheck.addEventListener("mouseout", function () {
-    imgCheck.src = "./icon/circle-regular.svg";
+    if (this.src.endsWith("circle-check-regular.svg")) {
+      imgCheck.src = "./icon/circle-regular.svg";
+    }
+  });
+  imgCheck.addEventListener("click", function () {
+    if (!this.src.endsWith("circle-check-blue.svg")) {
+      imgCheck.src = "./icon/circle-check-blue.svg";
+      content.style = " text-decoration: line-through; color: gray;";
+    } else {
+      imgCheck.src = "./icon/circle-regular.svg";
+      content.style = " text-decoration: ; color: darkslategrey;";
+    }
   });
   //star button action
   let imgStar = div.querySelectorAll("img")[1];
@@ -22,16 +46,6 @@ listItems.forEach((div) => {
       imgStar.src = "./icon/star-solid.svg";
     } else {
       imgStar.src = "./icon/star-regular.svg";
-    }
-  });
-  let content = div.querySelector("span");
-  content.addEventListener("click", () => {
-    // task.classList.toggle('hidden');
-    if (task.style.display === "none") {
-      task.style.display = "block";
-    } else {
-      task.style.display = "none";
-      console.log(div.textContent); //action on clicking each list item
     }
   });
 });
@@ -46,9 +60,9 @@ newToDo.addEventListener("input", () => {
     imgs[0].style.transform = "rotate(0)";
     imgs[1].style.display = "none";
   });
-  imgs[1].addEventListener("click",()=>{
+  imgs[1].addEventListener("click", () => {
     //这里添加增加一个新list的操作
-  })
+  });
 });
 
 menu.addEventListener("click", () => {
